b7866aecc137c37f150f937924d731cfebdab3e2
Thiago Martins <tmartins@coddera.com>
2017-05-08 11:29:00 -0300

    Fixed github api call (template)

--- chrome-verify-updates-job.sh
+++ chrome-verify-updates-job.sh
@@ -4,7 +4,7 @@ sudo apt update &&
 sudo apt list --upgradable | grep google-chrome-stable &&
 sudo apt upgrade google-chrome-stable &&
 CHROME_VERSION=$(google-chrome --version | awk '{print $3}') &&
-curl --user <username> \
+curl --user username:token \
     --header "Accept: application/vnd.github.manifold-preview" \
     --data "tag_name=${CHROME_VERSION}" \
     "https://api.github.com/repos/martinsthiago/chrome/releases"
