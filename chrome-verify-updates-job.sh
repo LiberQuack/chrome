3b06907a8a30eaef7cbe87c8849629cc11453013
Thiago Martins <rogue.thiago@gmail.com>
2017-05-22 02:10:27 -0300

    Update chrome-verify-updates-job.sh

--- chrome-verify-updates-job.sh
+++ chrome-verify-updates-job.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+docker run busybox echo "Checking if there is a new chrome version..."
 sudo apt update &&
 sudo apt list --upgradable | grep google-chrome-stable &&
+docker run busybox echo "Found new chrome version..." &&
 sudo apt upgrade google-chrome-stable &&
 CHROME_VERSION=$(google-chrome --version | awk '{print $3}') &&
+docker run busybox echo "Sending tag ${CHROME_VERSION} to github..." &&
 curl --user username:token \
     --header "Accept: application/vnd.github.manifold-preview" \
     --data "{\"tag_name\":\"${CHROME_VERSION}\"" \
