c4973d0e1ecaa81802ed789023ab9ab8801208d2
Thiago Martins <rogue.thiago@gmail.com>
2017-07-30 17:26:10 -0300

    Update chrome-verify-updates-job.sh

--- chrome-verify-updates-job.sh
+++ chrome-verify-updates-job.sh
@@ -4,7 +4,7 @@ docker run busybox echo "Checking if there is a new chrome version..."
 sudo apt update &&
 sudo apt list --upgradable | grep google-chrome-stable &&
 docker run busybox echo "Found new chrome version..." &&
-sudo apt upgrade google-chrome-stable &&
+sudo apt install -y google-chrome-stable &&
 CHROME_VERSION=$(google-chrome --version | awk '{print $3}') &&
 docker run busybox echo "Sending tag ${CHROME_VERSION} to github..." &&
 curl --user username:token \
