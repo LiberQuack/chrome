505fc33bb5791d1843e68a179e7beceda9c5cc11
Thiago Martins <rogue.thiago@gmail.com>
2017-07-30 22:38:36 -0300

    Added name on docker busybox

--- chrome-verify-updates-job.sh
+++ chrome-verify-updates-job.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-docker run busybox echo "Checking if there is a new chrome version..."
+docker run --rm --name browsers-job busybox echo "Checking if there is a new chrome version..."
 sudo apt update &&
 sudo apt list --upgradable | grep google-chrome-stable &&
-docker run busybox echo "Found new chrome version..." &&
+docker run --rm --name browsers-job busybox echo "Found new chrome version..." &&
 sudo apt install -y google-chrome-stable &&
 CHROME_VERSION=$(google-chrome --version | awk '{print $3}') &&
-docker run busybox echo "Sending tag ${CHROME_VERSION} to github..." &&
+docker run --rm --name browsers-job busybox echo "Sending tag ${CHROME_VERSION} to github..." &&
 curl --user username:token \
     --header "Accept: application/vnd.github.manifold-preview" \
     --data "{\"tag_name\":\"${CHROME_VERSION}\"" \
