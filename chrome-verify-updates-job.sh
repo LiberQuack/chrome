2c2eee765e9fe2180417fd260773c47779d735ef
Thiago Martins <rogue.thiago@gmail.com>
2017-07-31 10:57:42 -0300

    Changed container logger name browsers-job -> browsers-chrome-job

--- chrome-verify-updates-job.sh
+++ chrome-verify-updates-job.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-docker run --rm --name browsers-job busybox echo "Checking if there is a new chrome version..."
+docker run --rm --name browsers-chrome-job busybox echo "Checking if there is a new chrome version..."
 sudo apt update &&
 sudo apt list --upgradable | grep google-chrome-stable &&
-docker run --rm --name browsers-job busybox echo "Found new chrome version..." &&
+docker run --rm --name browsers-chrome-job busybox echo "Found new chrome version..." &&
 sudo apt install -y google-chrome-stable &&
 CHROME_VERSION=$(google-chrome --version | awk '{print $3}') &&
-docker run --rm --name browsers-job busybox echo "Sending tag ${CHROME_VERSION} to github..." &&
+docker run --rm --name browsers-chrome-job busybox echo "Sending tag ${CHROME_VERSION} to github..." &&
 curl --user username:token \
     --header "Accept: application/vnd.github.manifold-preview" \
     --data "{\"tag_name\":\"${CHROME_VERSION}\"" \
